9986a6bcf10bbfe19315fd1f251c61e1fe03b5d0
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-10 22:30:18 +0900

    Update isPrime.py

--- MillerRabinAlgorithm/isPrime.py
+++ MillerRabinAlgorithm/isPrime.py
@@ -1,19 +1,19 @@
 from random import randint
 
-def gcd(m,n):
+def gcd(m, n):
 	rem = 0
-	while n > 0:
+	while n>0:
 		rem = m%n
 		m = n
 		n = rem
 	return m
 
-def fastExp(m,e,n):
+def fastExp(m, e, n):
 	z = 1
 	while e>0:
 		while not e%2:
 			e = e//2
-			m = (m*m)%n
+			m - (m*m)%2
 		e -= 1
 		z = (z*m)%n
 	return z
@@ -24,15 +24,11 @@ def isPrime(n):
 	while not m%2:
 		m = m//2
 		k += 1
-
 	for i in range(100):
 		f = randint(2,n-1)
 		a = f*(n-2) if n>2 else 2
-		if gcd(a,n) != 1: return False
-
-		b = fastExp(a,m,n)
-		if b == 1 or b == (n-1): continue
-
+		
+		if gcd(a, n) != 1: return False
 		else:
 			for j in range(k-1):
 				b = (b*b)%n
