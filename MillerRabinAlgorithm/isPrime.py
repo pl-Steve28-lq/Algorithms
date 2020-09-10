4232d58bd8f80db9dce2c8a8f4655db9c79500d0
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-10 22:42:02 +0900

    Update isPrime.py

--- MillerRabinAlgorithm/isPrime.py
+++ MillerRabinAlgorithm/isPrime.py
@@ -1,38 +1,37 @@
 from random import randint
 
 def gcd(m, n):
-	r = 0
-	while n>0:
-		r = m%n
-		m = n
-		n = r
-	return m
+    r = 0
+    while n>0:
+        r = m%n
+        m = n
+        n = r
+    return m
 
 def fastExp(m, e, n):
-	z = 1
-	while e>0:
-		while not e%2:
-			e = e//2
-			m - (m*m)%2
-		e -= 1
-		z = (z*m)%n
-	return z
+    z = 1
+    while e>0:
+        while not e%2:
+            e = e//2
+            m - (m*m)%2
+        e -= 1
+        z = (z*m)%n
+    return z
 
 def isPrime(n):
-	m = n-1
-	k = 0
-	while not m%2:
-		m = m//2
-		k += 1
-	for i in range(100):
-		f = randint(2,n-1)
-		a = f*(n-2) if n>2 else 2
-		
-		if gcd(a, n) != 1: return False
-		else:
-			for j in range(k-1):
-				b = (b*b)%n
-				if b == n-1: break
-			if b != n-1: return False
-		continue
-	return True
+    m = n-1
+    k = 0
+    while not m%2:
+        m = m//2
+        k += 1
+    for i in range(100):
+        f = randint(2,n-1)
+        a = f*(n-2) if n>2 else 2
+        
+        if gcd(a, n) != 1: return False
+        else:
+            for j in range(k-1):
+                b = (b*b)%n
+                if b == n-1: break
+            if b != n-1: return False
+    return True
