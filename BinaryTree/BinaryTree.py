d282a9b89020fd6a594eb11efd21582296d51085
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-11 11:12:10 +0900

    Create BinaryTree.py

---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,17 @@
+class BinaryTree:
+    def __init__(self,item,left=None,right=None):
+        self.item = item
+        self.left = left
+        self.right = right
+
+def inorder(tree):
+    if type(N) == int: print(N)
+    elif N: inorder(N.left); print(N.item); inorder(N.right)
+
+def preorder(N):
+    if type(N) == int: print(N)
+    elif N: print(N.item); preorder(N.left); preorder(N.right)
+
+def postorder(N):
+    if type(N) == int: print(N)
+    elif N: postorder(N.left); postorder(N.right); print(N.item)
