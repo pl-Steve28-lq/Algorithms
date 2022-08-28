b649eb186845617e2fbb046c709a843c6533f4b8
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2022-08-28 13:28:33 +0900

    Update SieveOfEratosthenes.py

--- Prime Algorithm/SieveOfEratosthenes.py
+++ Prime Algorithm/SieveOfEratosthenes.py
@@ -1,16 +1,6 @@
 def primeSieve(n):
-  sieve = [False]*2 + [True]*(n-1)
-  for idx, e in enumerate(seive):
-    if e:
-      k = idx*2
-      while k <= n:
-        seive[k] = False
-        k += idx
-  return [x for (x, y) in enumerate(seive) if y]
-
-def primeSieveWithSlicing(n):
-  sieve = [False]*2 + [True]*(n-1)
-  for i in range(2, int(n**.5 + 1.5)):
+  sieve = [0]*2 + [1]*(n-1)
+  for i in range(2, int(n**.5)+1):
     if sieve[i]:
-      sieve[i*2::i] = [False] * ((n-i) // i)
-  return [x for x in range(n+1) if sieve[x]]
+      sieve[i*2::i] = [0] * (n//i-1)
+  return [x for x, s in enumerate(sieve) if s]
