ec6b1e22b7c64137123a7a87d0e83d1864ce0f56
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2022-08-28 14:48:34 +0900

    Update HeapDijkstra.py

--- Searching Algorithm/GraphSearch/HeapDijkstra.py
+++ Searching Algorithm/GraphSearch/HeapDijkstra.py
@@ -1,55 +1,19 @@
-import heapq
-Inf = 999999999
+from heapq import*
+inf = float('inf')
 
-def heapSort(L):
-  _ = []
-  res = []
-  for i in L: heapq.heappush(_, i)
-  while _: res.append(heapq.heappop(_))
-  return res
+N = 5
+graph = {1: [(6, 3), (3, 4)], 2: [(3, 1)], 3: [(2, 4)], 4: [(1, 2), (1, 3)], 5: [(4, 2), (2, 4)]}
 
-def Node(val):
-  return [Inf, val, []]
+def dijkstra(start, end):
+  dist = [inf]*(N+1)
+  heap = [(0, start)]
+  while heap:
+    cur_cost, node = heappop(heap)
+    for cost, i in graph[node]:
+      c = cur_cost + cost
+      if c < dist[i]:
+        dist[i] = c
+        heappush(heap, (c, i))
+  return dist[end]
 
-# node = (cost: int, value: int, neighbors: tuple[n_cost: int, n_node: node])
-def dijkstra(tnodes, nodes, prev={}):
-  if not nodes: return tnodes, prev
-  node = heapq.heappop(nodes)
-  cost, value, neighbors = node
-
-  neighbors = heapSort(neighbors)
-  for n_cost, n_node in neighbors:
-    val = n_node[1]
-
-    new_cost = cost + n_cost
-    if new_cost < tnodes[val][0]:
-      tnodes[val][0] = new_cost
-      prev[val] = value
-
-  return dijkstra(tnodes, nodes, prev)
-
-def main():
-  Start = Node(0)
-  Node1 = Node(1)
-  Node2 = Node(2)
-  Node3 = Node(3)
-  Node4 = Node(4)
-  End = Node(5)
-
-  Start[0] = 0
-  Start[2] = [(5, Node1), (8, Node2), (10, Node3)]
-  Node1[2] = [(2, Node4), (1, Node2), (5, End)]
-  Node2[2] = [(3, Node3), (6, End)]
-  Node4[2] = [(2, End)]
-
-  nodes = [Start, Node1, Node2, Node3, Node4, End]
-  tnodes, prev = dijkstra(tuple(nodes), nodes)
-
-  root = []
-  i = 5
-  while i != 0:
-    root.append(i)
-    i = prev[i]
-  print(tnodes[5][0], root)
-
-if __name__ == '__main__': main()
+print(dijkstra(1, 2))
